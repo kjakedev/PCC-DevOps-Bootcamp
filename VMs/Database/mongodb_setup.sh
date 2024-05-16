420449016245e2d0cba7798037fda75e6a9bf544
Desktop-Kevin <kevinjake.dev@gmail.com>
2024-05-16 18:53:45 +0800

    Add database Vagrantfile setup

---
+++ VMs/Database/mongodb_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+
+sudo apt-get install -y gnupg curl
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-5.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-5.0.gpg \
+   --dearmor
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-5.0.gpg ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-database hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+sudo systemctl daemon-reload
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
+
