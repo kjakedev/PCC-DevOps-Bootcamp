8f90a0a54d938ae6f1f067f643086c65b1d1a87b
Kevin Jake <kevin_jake_04@yahoo.com>
2024-07-05 23:16:50 +0800

    add terraform demo

---
+++ Terraform/Demo/variables.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+  default = "/root/pets.txt"
+}
+variable "content" {
+  default = "We love pets!"
+}
+variable "prefix" {
+  default = "Mrs"
+}
+variable "separator" {
+  default = "."
+}
+variable "length" {
+  default = 2
+}
